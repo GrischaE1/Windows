0f5229d255abf8d66f25b10c68e784283d260ca0
GrischaE1 <54313015+GrischaE1@users.noreply.github.com>
2021-06-07 16:08:22 +0200

    Add files via upload

---
+++ WUfB/SearchWindowsUpdates.ps1
@@ -0,0 +1,348 @@
+﻿##########################################################################################
+# You running this script/function means you will not blame the author(s) if this breaks your stuff. 
+# This script/function is provided AS IS without warranty of any kind. Author(s) disclaim all 
+# implied warranties including, without limitation, any implied warranties of merchantability or of 
+# fitness for a particular purpose. The entire risk arising out of the use or performance of the sample 
+# scripts and documentation remains with you. In no event shall author(s) be held liable for any damages 
+# whatsoever (including, without limitation, damages for loss of business profits, business interruption, 
+# loss of business information, or other pecuniary loss) arising out of the use of or inability to use 
+# the script or documentation. Neither this script/function, nor any part of it other than those parts 
+# that are explicitly copied from others, may be republished without author(s) express written permission. 
+# Author(s) retain the right to alter this disclaimer at any time.
+##########################################################################################
+
+
+
+##########################################################################################
+# Name: WindowsUpdate.ps1
+# Version: 0.1
+# Date: 18.05.2021
+# Created by: Grischa Ernst gernst@vmware.com
+#
+# Description
+# - This Script will provide you a granual control over Window Updates - especially if you using Windows Update for Business
+# - This includes
+#     1. Download updates at any time, to reduce the installation time
+#     2. Install updates during a weekly maintanance window
+#     3. Download and install updates
+# - Updates that require a reboot, will reboot during maintanance window - but will not automatically reboot if no MW is configured
+# - Registry values in the "HKLM:\SOFTWARE\Policies\Custom\Windows Update" hive are used to configure the script
+#
+# Caution
+# This script is using the PSWindowsUpdate module - which will be installed automatically but requires internet connectivity
+#
+###################################
+# Registry values:
+# Name            Value                          Description
+# DirectDownload 
+#                 True                           Will download update independend of the maintanance window
+#                 False                          Will not start download before installation
+#
+# HiddenUpdates
+#                 e.g. KB4023057,KB5003173       Hide specific updates to make sure the updates are not installed
+#
+#
+# UnHiddenUpdates
+#                 e.g. KB4023057,KB5003173       Un-hide specific updates to make sure the updates are availible for installation again
+#
+#
+# LastInstallationDate 
+#                 e.g. 18/05/2021 03:24          Last time where update installation was tried
+#
+#
+#
+# MaintanaceWindow
+#                 True                           Will install updates only during maintanance window
+#                 False                          Will install updates whenever possible
+# 
+#
+# MWDay
+#                 e.g. 1 for Monday
+#                 e.g. 2 for Tuesday             Will install updates only on a spefic day of the week
+#
+#
+# MWStartTime
+#                 08:00                          Will start the installation only after this time - make sure you are using 24 hours format
+#
+#
+# MWStopTime
+#                 09:30                          Will not install updates after this time - make sure you are using 24 hours format
+#
+#
+# UseMicrosoftUpdate
+#                 True                           Will force to use Microsoft Update - will ignore WSUS settings
+#                 False                          Will use configured settings
+#
+##########################################################################################
+#                                    Changelog 
+#
+# 0.1 - Inital creation
+##########################################################################################
+
+##########################################################################################
+#                                    Param 
+#
+#define the log folder if needed - otherwise delete it or set it to ""
+param(
+		[string]$LogPath
+	)
+
+##########################################################################################
+
+
+##########################################################################################
+#                                   Define varibales
+
+#define the log folder if needed - otherwise delete it or set it to ""
+if$(LogPath){$Log = $LogPath}
+
+if($Log){
+if((Test-Path (Split-Path -Parent $Log)) -eq $false){New-Item (Split-Path -Parent $Log) -ItemType Directory}
+Start-Transcript -Path $Log -Append}
+Write-Output "###############################################################################"
+Write-Output "Start Windows Update on $(Get-Date -Format "dd/MM/yyyy HH:mm")"
+
+Try { Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force -ErrorAction Stop } Catch {}
+
+#Enable this if you want to install the latest version of PSWindowsUpdate
+<#
+$PackageProvider = Get-PackageProvider -ListAvailable
+$NuGetInstalled = $false
+foreach ($item in $PackageProvider.name)
+{
+   if($item -eq “NuGet”)
+   {
+       $NuGetInstalled = $tru
+   }
+}
+if($NuGetInstalled = $false)
+{
+   Install-PackageProvider NuGet -Force
+}
+else{
+Get-PackageProvider -Name NuGet -Force
+Write-Output “NuGet is already installed”}
+
+$ModuleInstalled = Get-Module PSWindowsUpdate
+if(!$ModuleInstalled)
+{
+   Install-Module -Name PSWindowsUpdate -Force
+}
+else{Write-Output “PSWindowsUpdate Module is already installed”}
+
+#>
+
+Import-Module PSWindowsUpdate
+
+  #####
+  #Status Codes
+  #------- Update is requested
+  #-D----- Update is downloaded
+  #--I---- Update is installed
+  #---H--  Update is hidden
+  #-----U- Update is uninstallable
+  #------B Update is beta
+
+
+  #Get settings from Registry
+  $Settings =  Get-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Custom\WindowsUpdate'
+
+  #Hide unwanted updates
+  If($settings.HiddenUpdates)
+  {
+     Hide-WindowsUpdate -KBArticleID $settings.HiddenUpdates -Hide -Confirm:$false
+  }
+
+  #Allow hidden update again
+  If($settings.UnHiddenUpdates)
+  {
+     UnHide-WindowsUpdate -KBArticleID $settings.UnHiddenUpdates -Confirm:$false
+  }
+  
+  #To make sure to overwrite the WSUS settings
+  if($Settings.UseMicrosoftUpdate -eq $true)
+  {
+    #Add Microsoft Update as Update provider
+    Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -AddServiceFlag 7 -Confirm:$false | Out-Null
+    $AllUpdates = Get-WindowsUpdate -MicrosoftUpdate
+  }
+  else
+  {
+      #Get all updates with the configured update source
+      $AllUpdates = Get-WindowsUpdate
+  }
+
+  #If Download is independent of installation - Download all Updates
+  If($Settings.DirectDownload -eq $True)
+  {
+    #get all updates that are not downloaded
+    $NotDonwloadedUpdates = $AllUpdates | Where-Object {$_.Status -notlike "*D*"}
+    foreach($Update in $NotDonwloadedUpdates)
+    {
+        $UpdateKB = $update.KB
+        
+        #Use Microsoft Update as update source
+        if($Settings.UseMicrosoftUpdate -eq $true)
+        {
+            $result = Get-WindowsUpdate -Download -KBArticleID $UpdateKB -MicrosoftUpdate -Confirm:$false
+        }
+        else
+        {
+            $result = Get-WindowsUpdate -Download -KBArticleID $UpdateKB -Confirm:$false
+        }
+
+        #Check if download was successfull
+        if($result.result -contains "Downloaded")
+        {
+            "KB $($UpdateKB) downloaded"
+        }
+        #retry download
+        else
+        {
+            #Use Microsoft Update as update source
+            if($Settings.UseMicrosoftUpdate -eq $true)
+            {
+                $result = Get-WindowsUpdate -ForceDownload -KBArticleID $UpdateKB -MicrosoftUpdate -Confirm:$false
+            }
+            else
+            {
+                $result = Get-WindowsUpdate -ForceDownload -KBArticleID $UpdateKB -Confirm:$false
+            }
+            
+            #Check again download status
+            if($result.result -contains "Downloaded")
+            {
+                "KB $($UpdateKB) downloaded"
+            }
+            else
+            {
+                "KB $($UpdateKB) NOT downloaded"
+            }
+        }
+    }
+   
+  }
+  
+
+  #Install Updates
+  #Check if Maintanance Window is enabled
+  if($Settings.MaintanaceWindow -eq $true)
+  {
+    #Get current time
+    $CurrentDate = Get-Date
+    $Day = $CurrentDate.DayOfWeek.value__
+    $Hour = Get-Date -Format HH 
+    $Minute = Get-Date -Format mm
+
+    #Get start time
+    $MWStartHour = $Settings.MWStartTime.Substring(0,2)
+    $MWStartMinute = $Settings.MWStartTime.Remove(0,3)
+    
+    #Get stop time
+    $MWStopHour = $Settings.MWStopTime.Substring(0,2)
+    $MWStopMinute = $Settings.MWStopTime.Remove(0,3)
+
+    #Check if installation day was set - if not, updates will be installed everyday 
+    if($Settings.MWDay)
+    {
+      $TagetDay = $Settings.mwday
+    }
+    else
+    {
+      $TagetDay = $day
+    }
+   
+    #Check if current time is in maintanance window
+    if( $day -eq $TagetDay -and (($Hour -ge $MWStartHour -and $Minute -ge $MWStartMinute) -and ($Hour -lt $MWStopHour -or ($Hour -eq $MWStopHour -and  $Minute -lt $MWStopMinute))) )
+    {
+       
+        #Force to use Microsoft update - this will also overwrite current WSUS settings
+        if($Settings.UseMicrosoftUpdate -eq $true)
+        {
+            $InstallableUpdates = Get-WindowsUpdate -MicrosoftUpdate
+            foreach($Update in $InstallableUpdates)
+            {
+                $UpdateKB = $update.KB
+        
+                $Installation =  Install-WindowsUpdate -Install -KBArticleID $UpdateKB -Confirm:$false -MicrosoftUpdate -AutoReboot
+                
+                if($result.result -contains "Installed")
+                {
+                    "$($UpdateKB) installed"
+                }
+                else
+                {
+                    "$($UpdateKB) NOT installed"
+                }
+            }
+        }
+        #Use configured settings
+        else
+        {
+            $InstallableUpdates = Get-WindowsUpdate 
+            foreach($Update in $InstallableUpdates)
+            {
+                $UpdateKB = $update.KB
+        
+                $Installation =  Install-WindowsUpdate -Install -KBArticleID $UpdateKB -Confirm:$false -AutoReboot
+                
+                if($Installation.result -contains "Installed")
+                {
+                    "$($UpdateKB) installed"
+                }
+                else
+                {
+                    "$($UpdateKB) NOT installed"
+                }
+             }        
+          }
+          Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Custom\Windows Update' -Name 'LastInstallationDate' -Value $(Get-Date -Format "dd/MM/yyyy HH:mm")
+    }
+  }
+  #If no Maintanance Window is configured, just install the updates at any time
+  else
+  {
+        if($Settings.UseMicrosoftUpdate -eq $true)
+        {
+            $InstallableUpdates = Get-WindowsUpdate -MicrosoftUpdate
+            
+            foreach($Update in $InstallableUpdates)
+            {
+                $UpdateKB = $update.KB
+                $Installation =  Install-WindowsUpdate -Install -KBArticleID $UpdateKB -Confirm:$false -MicrosoftUpdate -IgnoreReboot
+            
+                if($Installation.result -contains "Installed")
+                {
+                    "$($UpdateKB) installed"
+                }
+                else
+                {
+                    "$($UpdateKB) NOT installed"
+                }
+            }
+        }
+        else
+        {
+            $InstallableUpdates = Get-WindowsUpdate
+
+            foreach($Update in $InstallableUpdates)
+            {
+                $UpdateKB = $update.KB
+                $Installation =  Install-WindowsUpdate -Install -KBArticleID $UpdateKB -Confirm:$false -MicrosoftUpdate -IgnoreReboot
+            
+                if($Installation.result -contains "Installed")
+                {
+                    "$($UpdateKB) installed"
+                }
+                else
+                {
+                    "$($UpdateKB) NOT installed"
+                }
+            }
+        }
+        Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Custom\Windows Update' -Name 'LastInstallationDate' -Value $(Get-Date -Format "dd/MM/yyyy HH:mm")
+  }
+
+Write-Output "Stop Windows Update on $(Get-Date -Format "dd/MM/yyyy HH:mm")"
+Write-Output "###############################################################################"
+if($Log){Stop-Transcript}
---
+++ WUfB/install.ps1
@@ -0,0 +1,36 @@
+﻿
+##########################################################################################
+#                                    Param 
+#
+#define the log folder if needed - otherwise delete it or set it to ""
+param(
+		[string]$LogPath,
+        [string]$InstallDir,
+        [int]$UpdateInterval
+	)
+
+
+#Create action
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -file ""$($InstallDir)\SearchWindowsUpdates.ps1"" -LogPath ""$($LogPath)\WindowsUpdate.log""" 
+
+#create triggers
+$starttime=(get-date)
+$TimeSpan = New-TimeSpan -Minutes $UpdateInterval
+
+$trigger = @()
+$trigger += New-ScheduledTaskTrigger -Once -At $startTime -RepetitionInterval $TimeSpan
+$trigger += New-ScheduledTaskTrigger -AtStartup
+
+#Use System account
+$User = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+#Settings to make sure the task can run everytime
+$settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -MultipleInstances Queue -DontStopOnIdleEnd -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+#Create the new scheduled task
+Register-ScheduledTask -TaskName "Search Windows Updates" -Action $action -Trigger $trigger -Principal $user  -Settings $settings –Force
+
+
+New-Item -Path C:\Windows -Name 'CustomUpdate' -ItemType Directory -Force
+Copy-Item "$PSScriptRoot\SearchWindowsUpdates.ps1" -Destination $InstallDir -Force
+Copy-Item "$PSScriptRoot\PSWindowsUpdate" -Destination 'C:\Program Files\WindowsPowerShell\Modules\' -Recurse -Force
