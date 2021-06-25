833001825948f163406e4d57758e6debf266e724
GrischaE1 <54313015+GrischaE1@users.noreply.github.com>
2021-06-25 15:58:06 +0200

    Update profilecreator.resources.ps1
    updated for multi day MW support

--- WUfB/GUI/profilecreator.resources.ps1
+++ WUfB/GUI/profilecreator.resources.ps1
@@ -1,8 +1,20 @@
-@{
+##########################################################################################
+# You running this script/function means you will not blame the author(s) if this breaks your stuff. 
+# This script/function is provided AS IS without warranty of any kind. Author(s) disclaim all 
+# implied warranties including, without limitation, any implied warranties of merchantability or of 
+# fitness for a particular purpose. The entire risk arising out of the use or performance of the sample 
+# scripts and documentation remains with you. In no event shall author(s) be held liable for any damages 
+# whatsoever (including, without limitation, damages for loss of business profits, business interruption, 
+# loss of business information, or other pecuniary loss) arising out of the use of or inability to use 
+# the script or documentation. Neither this script/function, nor any part of it other than those parts 
+# that are explicitly copied from others, may be republished without author(s) express written permission. 
+# Author(s) retain the right to alter this disclaimer at any time.
+##########################################################################################
+
+& { $BinaryFormatter = New-Object -TypeName System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
+ @{ 
 'ButtonCreate.Name' = 'ButtonCreate'
 'ComboBoxDay.Name' = 'ComboBoxDay'
-'TextBox1.Name' = 'TextBox1'
-'TextBox2.Name' = 'TextBox2'
 'ComboBoxMMW.Name' = 'ComboBoxMMW'
 'ComboBoxDL.Name' = 'ComboBoxDL'
 'Label1.Name' = 'Label1'
@@ -16,13 +28,12 @@
 'Label7.Name' = 'Label7'
 'ComboBoxMSUpdate.Name' = 'ComboBoxMSUpdate'
 'Label8.Name' = 'Label8'
-'$this.Name' = 'Form1'
-'CheckBox1.Name' = 'CheckBox1'
-'CheckBox2.Name' = 'CheckBox2'
 'ClipCheckBox.Name' = 'ClipCheckBox'
 'XMLCheckBox.Name' = 'XMLCheckBox'
-'MaskedTextBox1.Name' = 'MaskedTextBox1'
-'DateTimePicker1.Name' = 'DateTimePicker1'
 'DateTimePickerStart.Name' = 'DateTimePickerStart'
 'DateTimePickerEnd.Name' = 'DateTimePickerEnd'
+'ListBoxDay.Name' = 'ListBoxDay'
+'Label9.Name' = 'Label9'
+'$this.Name' = 'Form1'
+}
 }
