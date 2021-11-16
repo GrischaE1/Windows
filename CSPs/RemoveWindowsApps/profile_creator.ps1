2ca6e46dfab89d478dc1f9592328640fd4297153
GrischaE1 <54313015+GrischaE1@users.noreply.github.com>
2021-11-16 14:19:55 +0100

    Update profile_creator.ps1

--- CSPs/RemoveWindowsApps/profile_creator.ps1
+++ CSPs/RemoveWindowsApps/profile_creator.ps1
@@ -21,22 +21,29 @@
 #
 # Description
 # - This Script will a custom device profile to uninstall Windows apps
-# - Just export the App names you want to uninstall via PowerShell:
-#
+# - Just export the App names you want to uninstall via PowerShell
+# 
+# For User Apps:
 #  Get-AppxPackage | Where-Object {$_.NonRemovable -eq $false -and $_.IsFramework -eq $false} | select PackageFamilyName
 #
-# Copy and paste the PackageFamilyName to a txt file and change the $inputfile to your file
-# If needed change the outputfile
+# For Device Apps:
+# Get-AppxProvisionedPackage -online | select PackageName
 #
-# Caution
-# This script is using the PSWindowsUpdate module - which will be installed automatically but requires internet connectivity
+# Copy and paste the PackageFamilyName to a txt file and change the $inputfile to your file
+# If needed change the input/outputfile
 #
 ###################################
+
+
+#############################################################################################################################
+# User profile - will uninstall the app for the current user
 
 
 $inputfile = "C:\Temp\Windows11_21H2_Get-AppxPackage.txt"
 
 $Apps = Get-Content $inputfile
+
+Clear-Variable Temp,RemoveApp -Force -ErrorAction SilentlyContinue
 
 foreach($app in $apps)
 {
@@ -46,7 +53,7 @@ foreach($app in $apps)
         <CmdID>$($guid.Guid)</CmdID>
         <Item>
             <Target>   
-                <LocURI>./Device/Vendor/MSFT/EnterpriseModernAppManagement/AppManagement/AppStore/$($app)</LocURI>
+                <LocURI>./User/Vendor/MSFT/EnterpriseModernAppManagement/AppManagement/AppStore/$($app)</LocURI>
             </Target>
         </Item>
     </Delete>
@@ -55,7 +62,52 @@ foreach($app in $apps)
     $temp += $RemoveApp
 
 }
-    
     #Export to XML
+
     $OutPut = $ExecutionContext.InvokeCommand.ExpandString($temp)
-    $OutPut  | Out-File "C:\Temp\WindowsRemoveApps.xml" -Force
+    $OutPut  | Out-File "C:\Temp\WindowsRemoveApps_User.xml" -Force
+
+
+
+
+#############################################################################################################################
+# Device profile - will uninstall the app for all users
+
+
+$inputfile = "C:\Temp\Windows11_21H2_Get-AppxProvisionedPackage.txt"
+
+$Apps = Get-Content $inputfile
+
+Clear-Variable Item,NewItem,Temp,RemoveApp -Force -ErrorAction SilentlyContinue
+
+foreach($app in $apps)
+{
+    
+
+    $NewItem = '
+   <Item>
+      <Target>
+         <LocURI>./Device/Vendor/MSFT/EnterpriseModernAppManagement/AppManagement/RemovePackage</LocURI>
+      </Target>
+      <Meta><Format xmlns="syncml:metinf">xml</Format></Meta>
+      <Data>
+         <Package Name="$($app)" RemoveForAllUsers="1" />
+      </Data>
+   </Item>
+        '
+
+    $Item += $ExecutionContext.InvokeCommand.ExpandString($NewITem)
+
+}
+$guid = [guid]::NewGuid()
+
+$temp = "<Exec>
+   <CmdID>$($guid)</CmdID>
+    $($Item)
+</Exec>
+"
+$temp
+    #Export to XML
+
+    $OutPut = $ExecutionContext.InvokeCommand.ExpandString($temp)
+    $OutPut  | Out-File "C:\Temp\WindowsRemoveApps_Device.xml" -Force
